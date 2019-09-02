da5f8804d229fcfb48c598c8874975c7684e4ec1
Dani <daniel.redondo@gmail.com>
2019-09-02 23:09:09 +0200

    Commit inicial

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        // Time.deltaTime: Devuelve el tiempo que tarda en ejecutar cada frame
+        // Lo usamos para que la nave se mueva a la misma velocidad en computadoras lentas y rápidas
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var newXPos = transform.position.x + deltaX;
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        var newYPos = transform.position.y + deltaY;
+
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+}
