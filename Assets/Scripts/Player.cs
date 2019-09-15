6d6586989db98c3fb5b9fd55ff61c6234f533b06
Dani <daniel.redondo@gmail.com>
2019-09-15 22:12:56 +0200

    Movido paquete de assets a otro sitio. Ahora la nave dispara

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,17 +6,35 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float padding = 1f;
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] float projectileSpeed = 10f;
+
+    float xMin;
+    float yMin;
+    float xMax;
+    float yMax;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetUpMoveBoundaries();
     }
 
     // Update is called once per frame
     void Update()
     {
         Move();
+        Fire();
+    }
+
+    private void Fire()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity) as GameObject;
+            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+        }
     }
 
     private void Move()
@@ -24,10 +42,25 @@ public class Player : MonoBehaviour
         // Time.deltaTime: Devuelve el tiempo que tarda en ejecutar cada frame
         // Lo usamos para que la nave se mueva a la misma velocidad en computadoras lentas y rápidas
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-        var newXPos = transform.position.x + deltaX;
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        var newYPos = transform.position.y + deltaY;
 
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
         transform.position = new Vector2(newXPos, newYPos);
+    }
+
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        // ViewportToWorldPoint: Coge la posicion relativa de la camara y lo transforma a una posicion global
+        // Por ej: la posicion "0, 0" de la camara podria ser la posicion global "-5.61, 12.23"
+
+        Vector3 bottomLeft = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0));
+        Vector3 topRight = gameCamera.ViewportToWorldPoint(new Vector3(1, 1, 0));
+
+        xMin = bottomLeft.x + padding;
+        yMin = bottomLeft.y + padding;
+        xMax = topRight.x - padding;
+        yMax = topRight.y - padding;
     }
 }
